1a2243a331ea45be1a7dcc0826bdfc0757533fec
Davidson <93540680+Conscious-Mind@users.noreply.github.com>
2022-11-01 19:30:03 +0530

    Initial Commit

---
+++ src/main/kotlin/prefix_sum/1480_running_sum_of_1d_array.kt
@@ -0,0 +1,41 @@
+package prefix_sum
+
+fun main() {
+    val nums = intArrayOf(1, 2, 3, 4)
+    println(runningSum(nums).contentToString())
+}
+
+//private fun runningSum(nums: IntArray): IntArray {
+//    val sumarray = mutableListOf<Int>()
+//    var sum = 0
+//
+//    for (i in nums.indices) {
+//        sum += nums[i]
+//        sumarray.add(sum)
+//    }
+//    return sumarray.toIntArray()
+//}
+
+// 58%, 60%
+//private fun runningSum(nums: IntArray): IntArray {
+//    var prev = 0
+//    for (i in nums.indices) {
+//        nums[i] += prev
+//        prev = nums[i]
+//    }
+//    return nums
+//}
+
+// 90%, 90%
+//private fun runningSum(nums: IntArray): IntArray {
+//    for (i in nums.indices) {
+//        if (i != 0) {
+//            nums[i] += nums[i-1]
+//        }
+//    }
+//    return nums
+//}
+
+private fun runningSum(nums: IntArray): IntArray {
+    return nums.runningReduce { acc, i ->  acc + i}.toIntArray()
+}

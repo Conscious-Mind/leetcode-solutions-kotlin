d38c03e28abbdfcb7e7fd66f0d70a5170b8c3c64
Davidson <93540680+Conscious-Mind@users.noreply.github.com>
2022-11-01 23:51:28 +0530

    solved in 01-11-2022

--- src/main/kotlin/playground.kt
+++ src/main/kotlin/playground.kt
@@ -1,32 +1,3 @@
-class Solution {
-    fun pivotIndex(nums: IntArray): Int {
-        var left = 0
-        var right = 0
+fun main() {
 
-        for (i in nums.indices) {
-            if (i == 0) {
-                left = 0
-                right = findSumBetweenIndex(i + 1, nums.size, nums)
-            } else if (i == nums.size - 1) {
-                right = 0
-                left = findSumBetweenIndex(0, i, nums)
-            } else {
-                left = findSumBetweenIndex(0, i, nums)
-                right = findSumBetweenIndex(i + 1, nums.size, nums)
-            }
-            if (left == right) {
-                return i
-            }
-        }
-        return -1
-
-    }
-
-    private fun findSumBetweenIndex(start: Int, end: Int, nums: IntArray): Int {
-        var sum = 0
-        for (i in start until end) {
-            sum += nums[i]
-        }
-        return sum
-    }
 }
---
+++ src/main/kotlin/string/205_isomorphic_strings.kt
@@ -0,0 +1,53 @@
+package string
+
+fun main() {
+    val string1 = "egg"
+    val string2 = "add"
+    println(isIsomorphic(string1, string2))
+}
+
+//private fun isIsomorphic(s: String, t: String): Boolean {
+//    if (s.length != t.length)
+//        return false
+//
+//    val charMap1 = hashMapOf<Char, Int>()
+//    var charMap1Length = 1
+//    val charMap2 = hashMapOf<Char, Int>()
+//    var charMap2Length = 1
+//    val orderMap1 = hashMapOf<Int, Int>()
+//    val orderMap2 = hashMapOf<Int, Int>()
+//
+//    for (i in s.indices) {
+//        charMap1.putIfAbsent(s[i], charMap1Length++)
+//        charMap2.putIfAbsent(t[i], charMap2Length++)
+//        charMap1[s[i]]?.let { orderMap1.put(i, it) }
+//        charMap2[t[i]]?.let { orderMap2.put(i, it) }
+//    }
+//
+//    return orderMap1 == orderMap2
+//}
+
+
+/**
+ * Using hashMap
+ * for every index, puting the `s[index]`, `t[index]` in hashmp<Char, Char> if the key is not present.
+ * If for an index the value of the key(`s[index]`) is not equal to value from t string(`t[index]`) return false.
+ * Otherwise True.
+ */
+private fun isIsomorphic(s: String, t: String): Boolean {
+    if (s.length != t.length)
+        return false
+
+    val map = hashMapOf<Char, Char>()
+
+    for (i in s.indices) {
+        if (map.containsKey(s[i]) || map.containsValue(t[i])) {
+            if (map[s[i]] != t[i]) {
+                return false
+            }
+        } else {
+            map[s[i]] = t[i]
+        }
+    }
+    return true
+}
